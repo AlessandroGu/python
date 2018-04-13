2cd2df1d4ed0076a531f9d58abb44ff30334b242
AlessandroGu <924291390@qq.com>
2018-04-13 14:44:19 +0800

    first commit

---
+++ python.py
@@ -0,0 +1,2 @@
+# 关于如何安装python环境，详情见廖雪峰老师的文章，链接  https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000
+# 如何编辑python代码，(1).直接在cmd命令行窗口输入python，进入交互式环境，填代码就好。(2).打开sublime、nodepad等编辑器
